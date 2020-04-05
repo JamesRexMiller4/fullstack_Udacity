b5c7f203691416fd3b56120187cc4f8a6f07043e
James Miller <james.rex.miller.4@gmail.com>
2020-04-05 11:20:02 -0600

    Fix typo with data in render_template

--- todoapp/flask_todo_app.py
+++ todoapp/flask_todo_app.py
@@ -1,23 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import sqlalchemy
 
 app = Flask(__name__)
-app.congif['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/')
 def index():
   return render_template('index.html', data=[{
-    {
       'description': 'Todo 1'
-    }, 
-    {
+    }, {
       'description': 'Todo 2'
-    }, 
-    {
+    }, {
       'description': 'Todo 3'
-    }
-  }])
+    }])
 
 
 
