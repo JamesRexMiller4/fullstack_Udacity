05ad8ea404d2fe9b17d7a035721cd61939c3c0b9
James Miller <james.rex.miller.4@gmail.com>
2020-04-04 22:28:23 -0600

    Rename file

--- flask_hellllo_app.py
+++ flask_hellllo_app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/test2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -15,7 +16,8 @@ db.create_all()
 
 @app.route('/')
 def index():
-  return 'Hello World!'
+  person = Person.query.first()
+  return 'Hello ' + person.name
 
 
 if __name__ == '__main__':
