961e3886edb14843ae3b89a423a119f6339b594f
James Miller <james.rex.miller.4@gmail.com>
2020-04-06 22:44:53 -0600

    Add default false value for completed on todos table, rerun migration

--- todoapp/app.py
+++ todoapp/app.py
@@ -19,15 +19,12 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean(), nullable=False)
+  completed = db.Column(db.Boolean(), nullable=False, default=False)
   todo_list_id = db.Column(db.Integer, db.ForeignKey('todo_lists.id'), nullable=False)
 
 # def __repr__(self):
 #     return f'<Todo {self.id} {self.description}>'
 
-@app.route('/')
-def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
@@ -67,15 +64,22 @@ def set_completed(todo_id):
     
 @app.route('/delete/<todo_id>', methods=['DELETE'])
 def remove_todo(todo_id):
-    try:
-      Todo.query.filter_by(id=todo_id).delete()
-      db.session.commit()
-    except:
-      db.session.rollback()
-    finally:
-      db.session.close()
-    return jsonify({ 'success': True })
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
 
+@app.route('/lists/<todo_list_id>')
+def get_list_todos(todo_list_id):
+  return render_template('index.html', data=Todo.query.filter_by(todo_list_id=todo_list_id).order_by('id').all())
+
+@app.route('/')
+def index():
+  return redirect(url_for('get_list_todos', todo_list_id=1))
 
 
 if __name__ == '__main__':
